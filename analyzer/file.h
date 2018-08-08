8f2015f954731a38e7a7ab1c53ced1831aa59d56
melody1214 <wonjiwoong@hanmail.net>
2018-08-08 16:14:15 +0900

    analyzer: modify file paths

--- analyzer/file.h
+++ analyzer/file.h
@@ -1,12 +1,12 @@
-#include <stdio.h>
-#include <unistd.h>
 #include <fcntl.h>
+#include <linux/fiemap.h>
+#include <linux/fs.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/ioctl.h>
 #include <sys/types.h>
-#include <linux/fiemap.h>
-#include <linux/fs.h>
+#include <unistd.h>
 
 #define LBN_START 929523712
 #define SECTOR_SIZE 512
@@ -17,6 +17,9 @@
 
 #define PATH_READ_LOG "/home/melody/study/projects/trapfetch/logs/r."
 #define PATH_CANDIDATE_LOG "/home/melody/study/projects/trapfetch/logs/c."
+
+#define PATH_PF "/home/shared/prefetch/log/pf_"
+#define PATH_BP "/home/shared/prefetch/log/bp_"
 
 FILE *get_fd(char *path, char *dir, int flag);
 unsigned int get_logical_blk_addr(char *path);
