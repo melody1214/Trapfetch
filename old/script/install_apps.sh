aa89f458d2680961f36c1bc464280a80189680c8
melody1214 <jiwoongwon@hanyang.ac.kr>
2021-03-26 06:45:09 +0900

    old: add script for program installation

---
+++ old/script/install_apps.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Flight gear
+sudo add-apt-repository ppa:saiarcot895/flightgear
+
+# Speed dreams
+sudo add-apt-repository ppa:speed-dreams/ppa
+
+sudo apt update
+
---
+++ old/script/install_utils.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo apt install hdparm
+sudo apt install iowatcher
+sudo apt install blktrace
